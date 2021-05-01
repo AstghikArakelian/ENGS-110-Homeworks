ef17ef783ea6e81dafadb4d913720f81081152e7
Debian <debian@debian10.linuxvmimages.local>
2021-05-01 03:41:37 -0500

    Homework C, problem finally solved, could commit

--- ex1.py
+++ ex1.py
@@ -1,8 +1,9 @@
 
-def main():
-    N = int(input("Please insert the width of the rectangle"))
-    M = int(input("Please insert the height of the rectangle"))
-    
+
+def drawwithwhile(N, M):
+    print("Printing with for loop")
+
+   
     currrow = 0
     while (currrow < M):
         if(currrow == 0 or currrow == (M-1)):
@@ -17,8 +18,20 @@ def main():
 
                 currcol += 1
             print("", end="\n")
-                
-
         currrow += 1
+        drawwithfor(N, M)
 
+def drawwithfor(W, H):
+    print("Printing with for loop")
+    for(currrow == 1 or currrow == H):
+        print(W * "*")
+    else: 
+        for(currcol
+
+
+def main():
+    N = int(input("Please insert the width of the rectangle: "))
+    M = int(input("Please insert the height of the rectangle: "))
+    drawwithwhile(N, M)
+    drawwithfor(N, M)
 main()
